6ca8bbe609900a853950ef2aa78041aefd43afe3
tungatarovM <tungatarov.96@mail.ru>
2018-09-07 16:32:16 +0500

    add main game engine logic

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,34 @@ namespace BrainGames\Cli;
 use function \cli\line;
 use function \cli\prompt;
 
-function run()
+const LIVE_POINTS = 3;
+
+
+function startEngine($gameData, $gameRules) 
 {
-    line('Welcome to the Brain Games!');
-    $name = prompt('May I have your name');
-    line("Hello {$name}");
+    line("Welcome to the Brain Games!");
+    line("{$gameRules}");
+    $playerName = prompt("May I have your name");
+    line("Hello {$playerName}");
+    
+    for ($attempt = 0; $attempt < LIVE_POINTS; $attempt++) {
+        $currentGame = gameData();
+        $question = $currentGame['getQuestion'];
+        $correctAnswer = $currentGame['getCorrectAnswer'];
+
+        line("Question: {$question}");
+        $playersAnswer = prompt("Your answer:");
+
+        if ($playersAnswer === $correctAnswer) {
+            line("Correct!");
+            $userPoints = $userPoints + 1;
+        } else {
+            line("'{$playersAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
+            line("Let's try again {$playerName}");
+            return;
+        }     
+    }
+    return line("Congratulations {$playerName}");
+
+
 }
