07adeb7206ccedba98d82ac4d9d0fbc08bb3b21f
tungatarovM <tungatarov.96@mail.ru>
2018-09-13 22:12:02 +0500

    add brain-progression

---
+++ src/games/progression.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace BrainGames\Games\Progression;
+
+use function \BrainGames\Cli\startEngine;
+
+const PROGRESSION_GAME_RULES = 'What number is missing in this progression?';
+
+function mkSequence()
+{
+    $sequenceLength = 9;
+    $startNum = rand(0, 100);
+    $range = rand(0, 100);
+    $missedNumIndex = rand(0, $sequenceLength);
+    $sequence = [];
+
+    for ($index = 0; $index < $sequenceLength; $index++) {
+        if ($index === $missedNumIndex) {
+            $sequence[$index] = '..';
+        } else {
+            $sequence[$index] = $startNum + ($range * $index);
+        }
+    }
+
+    return [
+        'getSequence' => $sequence,
+        'getRange' => $range,
+        'getStartNum' => $startNum,
+        'getMissedNumIndex' => $missedNumIndex,
+        'getSequenceLength' => $sequenceLength,
+    ];
+}
+
+function toStr($sequenceObj)
+{
+    $seq = $sequenceObj['getSequence'];
+    return implode(' ', $seq);
+}
+
+function getMissedNum($sequenceObj)
+{
+    $seq = $sequenceObj['getSequence'];
+    $missedNumIndex = $sequenceObj['getMissedNumIndex'];
+    $startNum = $sequenceObj['getStartNum'];
+    $range = $sequenceObj['getRange'];
+
+    $previousNum = $seq[$missedNumIndex - 1];
+    $nextNum = $seq[$missedNumIndex + 1];
+
+    if ($missedNumIndex === 0) {
+        return $nextNum - $range;
+    }
+    return $previousNum + $range;
+}
+
+function getCorrectAnswer($sequenceObj)
+{
+    return getMissedNum($sequenceObj);
+}
+
+function gameRun()
+{
+    $gameData = function () {
+        $sequenceObj = mkSequence();
+        $question = toStr($sequenceObj);
+        $answer = getCorrectAnswer($sequenceObj);
+        return [
+            'getQuestion' => $question,
+            'getCorrectAnswer' => "{$answer}",
+        ];
+    };
+    return startEngine($gameData);
+}
